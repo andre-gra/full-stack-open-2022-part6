7cc562b434bfb0a10285f8f4aa02f4b241f1237d
andregra <andregra717@gmail.com>
2023-01-20 15:41:25 +0100

    add solution 6.13

--- redux-anecdotes/src/store/store.js
+++ redux-anecdotes/src/store/store.js
@@ -1,13 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+const anecdotesAtStart = []
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -43,6 +36,9 @@ const anecdoteSlice = createSlice({
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedNote
       )
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   },
 })
@@ -85,7 +81,7 @@ const filterSlice = createSlice({
   }
 })
 
-export const { createAnecdote, addVoteOf } = anecdoteSlice.actions
+export const { createAnecdote, addVoteOf, setAnecdotes } = anecdoteSlice.actions
 export const anecdoteReducer = anecdoteSlice.reducer
 
 export const { showVoteMessage, showCreateMessage, resetMessage } = notificationSlice.actions
